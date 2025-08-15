8ab60c8e214dbd10088407aa6cc77c4f30f85d27
yogirajiiitadev <yogirajchaudhar1976@gmail.com>
2025-08-15 16:23:50 +0530

    Updated api urls to deployed versions instead of localhost

--- apps/excalidraw-frontend/config.ts
+++ apps/excalidraw-frontend/config.ts
@@ -1,3 +1,7 @@
-export const HTTP_BACKEND = "http://localhost:3001";
-export const WS_URL = "ws://localhost:8080";
-export const HTTP_GEN_AI = "http://localhost:3003";
+// export const HTTP_BACKEND = "http://localhost:3001";
+// export const WS_URL = "ws://localhost:8080";
+// export const HTTP_GEN_AI = "http://localhost:3003";
+
+export const HTTP_BACKEND = "http://backend.scriblio.online";
+export const WS_URL = "ws://ws.scriblio.online";
+export const HTTP_GEN_AI = "http://agent.scriblio.online";
